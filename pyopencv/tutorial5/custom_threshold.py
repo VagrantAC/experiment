952cd32c4671a8b9cb01bbabe2effd9c14447e80
VagrantAC <vagrantac@126.com>
2024-01-07 20:05:26 +0800

    [PyOpencv] add tutorial

---
+++ pyopencv/tutorial5/custom_threshold.py
@@ -0,0 +1,17 @@
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread('./static/sudoku.jpg', 0)
+
+ret, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 4)
+th3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 17, 6)
+
+titles = ['Original', 'Global(v = 127)', 'Adaptive Mean', 'Adaptive Gaussian']
+images = [img, th1, th2, th3]
+
+for i in range(4):
+    plt.subplot(2, 2, i + 1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i], fontsize=8)
+    plt.xticks([]), plt.yticks([])
+plt.show()
